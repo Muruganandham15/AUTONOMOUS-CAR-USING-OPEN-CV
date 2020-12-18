477a55796e00fb7fdbf443f73c1404c99ba419f1
Muruganandham <vickymuruga6.mn@gmail.com>
2020-12-18 16:28:10 +0900

    Add files via upload

--- AUTO STEEING BY DETECTING LANES/DETECT LANES.py
+++ AUTO STEEING BY DETECTING LANES/DETECT LANES.py
@@ -20,57 +20,42 @@ while True:
     Canny_Img = canny(Img)                  # perform CANNY Edge Detection
     img_cropped = Region(Canny_Img)         # Crop the Image the to avoid Detecting Lines from Total View of the CAMERA
 
-    #lines_canny = cv2.HoughLinesP(Canny_Img, 1, np.pi / 180, 30, np.array([]), minLineLength=20,
-                                 # maxLineGap=28)          # Detect the Lines from the CANNY Img to display in FINAL Step
+    lines_canny = cv2.HoughLinesP(Canny_Img, 1, np.pi / 180, 9, np.array([]), minLineLength=6,
+                                  maxLineGap=11)          # Detect the Lines from the CANNY Img to display in FINAL Step
 
     lines_cropped = cv2.HoughLinesP(img_cropped, 1, np.pi / 180, 30, np.array([]), minLineLength=20,
                                     maxLineGap=28)   # Detect the Lines from the Cropped Img to Find ANGLE
 
     Left_lines, Right_lines = Find_angle(lines_cropped)
-    LeftAngleAvg,L_ack = left_angle_avg(Left_lines)
-    RightAngleAvg,R_ack = right_angle_avg(Right_lines)
+    LeftAngleAvg = left_angle_avg(Left_lines)
+    RightAngleAvg = right_angle_avg(Right_lines)
     print(LeftAngleAvg)
     print(RightAngleAvg)
-    """
-    if(LEFT_LINE_MAX > LeftAngleAvg) and (LeftAngleAvg > LEFT_LINE_MIN) and (RIGHT_LINE_MAX > RightAngleAvg) and (RightAngleAvg > RIGHT_LINE_MIN):
+    if not Right_lines and not Left_lines:
+        stop()
+        print("STOP")
+    elif not Right_lines:
+        go_right()
+        print("NO RIGHT LANE-TURN RIGHT")
+    elif not Left_lines:
+        go_left()
+        print("NO LEFT LANE-TURN LEFT")
+    elif(RIGHT_LINE_MAX > RightAngleAvg) and (RightAngleAvg > RIGHT_LINE_MIN) and (LEFT_LINE_MAX > LeftAngleAvg) and (LeftAngleAvg > LEFT_LINE_MIN):
         go_straight()
         print("STRAIGHT")
-    elif(LeftAngleAvg < LEFT_LINE_MIN) or (RightAngleAvg>RIGHT_LINE_MAX):
+    elif(RightAngleAvg>RIGHT_LINE_MAX) or (LeftAngleAvg < LEFT_LINE_MIN):
         go_left()
         print("LEFT")
-    elif(LeftAngleAvg > LEFT_LINE_MAX) or (RightAngleAvg<RIGHT_LINE_MIN):
+    elif(RightAngleAvg<RIGHT_LINE_MIN) or (LEFT_LINE_MAX < LeftAngleAvg):
         go_right()
         print("RIGHT")
+
     else:
         pass
-    """
-    if (L_ack is 1) and (R_ack is 1):
-        if(RIGHT_LINE_MAX > RightAngleAvg) and (RightAngleAvg > RIGHT_LINE_MIN) and (LEFT_LINE_MAX > LeftAngleAvg) and (LeftAngleAvg > LEFT_LINE_MIN):
-            go_straight()
-            print("STRAIGHT")
-        elif(RightAngleAvg>RIGHT_LINE_MAX) or (LeftAngleAvg < LEFT_LINE_MIN):
-            go_left()
-            print("LEFT")
-        elif(RightAngleAvg<RIGHT_LINE_MIN) or (LEFT_LINE_MAX < LeftAngleAvg):
-            go_right()
-            print("RIGHT")
-        else:
-            stop()
-            print("STOP")
-    elif R_ack is 0:
-        go_left()
-        print("LEFT_1")
-    elif L_ack is 0:
-        go_right()
-        print("RIGHT_1")
-    else:
-        pass
-        
-
-
-    line_img = display_lines_m(LANE_img, lines_cropped)             # Create An Image to display the Detected lines
+    line_img = display_lines_m(LANE_img, lines_canny)             # Create An Image to display the Detected lines
     Final_img = cv2.addWeighted(LANE_img, 0.8, line_img, 1, 1)    # Combine the Line Image and LANE Image to Display it
     StackedImages = stackImages(([Canny_Img, img_cropped], [line_img, Final_img]), 1)
     cv2.imshow("StackedImg", StackedImages)
-    cv2.waitKey(1)
+    cv2.waitKey(10)
+GPIO. cleanup()
     #break
